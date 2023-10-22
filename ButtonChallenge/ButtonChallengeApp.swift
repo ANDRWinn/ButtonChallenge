b5dc2c8249e103d0d09cd2126ce7c565a71931b6
Andrew Winn <winn.andrew.m@gmail.com>
2023-10-22 11:04:52 -0500

    Initial Commit

---
+++ ButtonChallenge/ButtonChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonChallengeApp.swift
+//  ButtonChallenge
+//
+//  Created by Andrew Winn on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
